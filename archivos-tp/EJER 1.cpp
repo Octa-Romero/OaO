a636c9c80f34e84140603f7529433fd4425926c2
Octa-Romero <166446204+Octa-Romero@users.noreply.github.com>
2025-04-23 23:21:14 -0300

    cambios

--- archivos-tp/EJER 1.cpp
+++ archivos-tp/EJER 1.cpp
@@ -3,37 +3,33 @@
 
 int i;
 
-main()
+void ingresarDatos(FILE *fp,char nombre[],char apellido[],int dni,int seguir)
 {
-	FILE *fp;
-	fp=fopen("usuarios.txt","w");
-	char nombre[35], apellido[35];
-	int dni,seguir,busqueda,opcion;
-	printf("EJECUCIONES DEL PROGRAMA\nIngresar datos (1) / Buscar Datos (2) / Salir del programa (3)\n");
-	scanf("%d",&opcion);
-	do{
-	switch(opcion)
-	{
-	case 1:
-	do
-	{
-	printf("Ingrese el nombre del usuario\n");
-	scanf("%s",nombre);
-	printf("Ingrese el apellido del usuario\n");
-	scanf("%s",apellido);
-	printf("Ingrese el dni del usuario\n");
-	scanf("%d",&dni);
-	printf("Desea seguir cargando datos? (1=si 0=no)\n");
-	scanf("%d",&seguir);
-	fprintf(fp,"%s",nombre);
-	fprintf(fp," %s",apellido);
-	fprintf(fp," %d\n",dni);
-}while(seguir!=0);
-	fclose(fp);
-	printf("SE HAN CARGAO LOS DATOS EN EL ARCHIVO CORRECTAMENTE (•-•)\n");
-	break;
-	
-	case 2:
+	fp=fopen("usuarios.txt","a");
+		do
+		{
+			printf("\nIngrese el nombre del usuario\n");
+			scanf("%s",nombre);
+			printf("Ingrese el apellido del usuario\n");
+			scanf("%s",apellido);
+			printf("Ingrese el dni del usuario\n");
+			scanf("%d",&dni);
+			printf("\nDesea seguir cargando datos? (1=si 0=no)\n");
+			scanf("%d",&seguir);
+			fprintf(fp,"%s %s %d\n\n",nombre,apellido,dni);
+		}while(seguir!=0);
+		
+			fclose(fp);
+			
+			printf("\nSE HAN CARGAO LOS DATOS EN EL ARCHIVO CORRECTAMENTE (â€¢-â€¢)\n");
+}
+
+
+
+
+void busquedaDni(FILE *fp,int busqueda,char *nombre,char *apellido)
+{
+	int encontrado=0,dni;
 	fp=fopen("usuarios.txt","r");
 	printf("\nIngrese el DNI del usuario que quiere buscar\n");
 	scanf("%d",&busqueda);
@@ -41,12 +37,44 @@ main()
 	{
 		if(busqueda==dni)
 		{
-			printf("El usuario con DNI %d es %s %s",dni,apellido,nombre);
+			printf("\nEl usuario con DNI %d es %s %s\n",dni,apellido,nombre);
+			encontrado=1;
 		}
 	}
+	
+	if(encontrado==0)
+	{
+		printf("\nNo existe un usuario con ese DNI dentro del archivo\n");
+	}
 	fclose(fp);
-	break;
+}
+
+main()
+{
+	FILE *fp;
+	char nombre[35], apellido[35];
+	int dni,seguir,busqueda,opcion;
+	do
+	{
+	printf("\nEJECUCIONES DEL PROGRAMA\nIngresar datos (1) / Buscar Datos (2) / Salir del programa (3)\nQue ejecucion desea realizar?:");
+	scanf("%d",&opcion);
+	switch(opcion)
+	{
+	case 1:
+			
+			ingresarDatos(fp,nombre,apellido,dni,seguir);
+			
+		break;
+	
+	case 2:
+			
+			busquedaDni(fp,busqueda,nombre,apellido);
+		
+		break;
 }
 }while(opcion!=3);
-printf("PROGRAMA FINALIZADO CON EXITO,HASTA LUEGO (•-•)\n");
+fp=fopen("usuarios.txt","r");
+fclose(fp);
+printf("\nPROGRAMA FINALIZADO CON EXITO,HASTA LUEGO (â€¢-â€¢)\n");
+
 }
