4c10d9dcc7e179a3495cb5fe637ced3b73542e7c
Octa-Romero <166446204+Octa-Romero@users.noreply.github.com>
2025-06-05 09:30:00 -0300

    subo ejer 1

---
+++ Java/TP1/ejer1.java
@@ -0,0 +1,18 @@
+
+public class ejer1 {
+
+	public static void main(String[] args) {
+		for(int i=1 ;i<=100;i++)				// hacer un ciclo for inicializando i como 1 e incrementarlo hasta 100 inclusive
+		{							
+			if(i % 2 > 0)						// condicion para saber si el numero es impar a traves del resto
+			{
+				System.out.println(i);			// en caso de ser verdadero imprimo el valor de dicho numero
+				
+			}else if(i % 2 == 0)				// condicion que se cumle cuando el numero es par
+			{
+				System.out.println("par");		// imprimo la palabra "par" 
+			}
+		}
+	}
+
+}
