f22fe7bafb739999da61fdc08f3b002243d6f8ad
Octa-Romero <166446204+Octa-Romero@users.noreply.github.com>
2025-06-05 09:30:47 -0300

    Update ejer1.java

--- Java/TP1/ejer1.java
+++ Java/TP1/ejer1.java
@@ -4,7 +4,7 @@ public class ejer1 {
 	public static void main(String[] args) {
 		for(int i=1 ;i<=100;i++)				// hacer un ciclo for inicializando i como 1 e incrementarlo hasta 100 inclusive
 		{							
-			if(i % 2 > 0)						// condicion para saber si el numero es impar a traves del resto
+			if(i % 2 > 0)					// condicion para saber si el numero es impar a traves del resto
 			{
 				System.out.println(i);			// en caso de ser verdadero imprimo el valor de dicho numero
 				
