a3325a34079411004c3fb56860c220ccd03023df
Octa-Romero <166446204+Octa-Romero@users.noreply.github.com>
2025-06-11 15:41:25 -0300

    Update ejer2.java

--- Java/TP1/ejer2.java
+++ Java/TP1/ejer2.java
@@ -2,22 +2,22 @@
 public class ejer2 {
 
 	public static void main(String[] args) {
-		for(int i=50 ;i<=100;i++)				
+		for(int i=50 ;i<=100;i++)	// hacer un ciclo for que recorra valores desde 50 hasta 100 inclusive	
 		{		
-		    boolean esPrimo=true;
-		    for(int j=2;j<i;j++)
+		    boolean esPrimo=true;	// declarar variable de tipo booleano para saber si es primo(inicialmente como true)
+		    for(int j=2;j<i;j++)	// hacer un ciclo for que recorra valores desde el 2 hasta el valor que tenga i en esta iteracion
 		    {
-		        if(i % j == 0)
+		        if(i % j == 0)		//condicion para averiguar si el numero es primo o no
 		        {
-		            esPrimo=false;
-		            System.out.print(j + " ");
+		            esPrimo=false;	// en caso de no ser primo, el valor del booleano pasara a ser false
+		            System.out.print(j + " "); //imprimo los divisores de dicho valor no primo
 		        }
 		    }
-		    if(esPrimo)
+		    if(esPrimo)	//al salir de la comparacion en el ciclo for anterior, verifico dicho valor booleano para saber si es primo
 		    {
-		        System.out.print("primo\n");
+		        System.out.print("primo\n"); //en caso de serlo, imprimo la palabra primo
 		    }else{
-		        System.out.print("\n");
+		        System.out.print("\n"); //en caso de no serlo, hago un salto de linea para que la salida de los mensajes salga uno abajo del otro
 		    }
 		}
 
