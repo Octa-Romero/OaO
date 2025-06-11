6ea70b4d3ce84b1bd34614f68b71a0dac8c65533
Octa-Romero <166446204+Octa-Romero@users.noreply.github.com>
2025-06-11 15:42:39 -0300

    Update ejer3.java

--- Java/TP1/ejer3.java
+++ Java/TP1/ejer3.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 public class ejer3 {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int numero;
-		boolean noPrimo;
-		do
+		Scanner scanner = new Scanner(System.in);	// inicializamos Scanner
+		int numero;			// declaro variable de tipo entero
+		boolean noPrimo;	// declaro variable de tipo booleano
+		do	// hago un ciclo do while
 		{
-			noPrimo=false;
-			System.out.print("Ingrese un numero:");
-			numero = scanner.nextInt();
-			for(int i=2;i<numero;i++)
+			noPrimo=false; // inicializo variable con valor false
+			System.out.print("Ingrese un numero:"); // pido al usuario un numero
+			numero = scanner.nextInt();// leo el numero que ingreso el usuario 
+			for(int i=2;i<numero;i++) // hacer un ciclo for que recorra valores desde el 2 hasta el numero ingresado(no incluido) 
 			{
-				if(numero % i == 0)
+				if(numero % i == 0)	// condicion para saber si dicho numero no es primo
 				{
-					noPrimo=true;
-					break;
+					noPrimo=true; // si se cumple dicha condicion, el valor del booleano pasa a ser true
+					break;	// al encontrar al menos un divisor del numero, salgo del ciclo
 				}
 			}
-		}while(numero<100 || noPrimo);
-		System.out.println("Saliendo del programa");
-		scanner.close();
+		}while(numero<100 || noPrimo); // condicion del ciclo do while que si no se cumple(numero mayor o igual a 100 y a la vez sea primo), se sale del ciclo
+		System.out.println("Saliendo del programa"); // mensaje que se muestra cuando se sale del ciclo
+		scanner.close(); // se cierra el scanner
 	}
 
 }
